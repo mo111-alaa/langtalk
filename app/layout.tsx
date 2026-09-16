a0260ce94058c4bef337ae36b077d36662c3a717
Mohammed ALAA <mohamedalaaabd111@gmail.com>
2026-09-16 14:40:18 +0300

    Add Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-// استخدام المسار المباشر بـ @ بعد ضبط tsconfig.json
 import { LocalizationProvider } from "@/components/ui/LocalizationProvider";
 
 export const metadata: Metadata = {
   title: "LangTalk",
   description: "Practice languages with real people",
-  verification: {
-    google: "r4hF0pN1a1sbAwI7I2aqz6auibkwdx9sH2rS92op_Og",
-  },
 };
 
 export default function RootLayout({
@@ -19,6 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" dir="ltr">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="r4hF0pN1a1sbAwI7I2aqz6auibkwdx9sH2rS92op_Og"
+        />
+      </head>
+
       <body>
         <LocalizationProvider>
           {children}
