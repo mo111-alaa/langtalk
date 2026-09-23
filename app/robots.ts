8c9729956eb808008d847f5f46df8bb06c41d352
Mohammed ALAA <mohamedalaaabd111@gmail.com>
2026-09-23 08:56:04 +0300

    add robots

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://language-with-anyone.vercel.app/sitemap.xml',
+    sitemap: 'https://lwa.vercel.app/sitemap.xml',
   }
 }
--- app/sitemp.ts
+++ app/sitemp.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://language-with-anyone.vercel.app',
+      url: 'https://lwa.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
